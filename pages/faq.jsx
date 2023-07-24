944cd1d7ecc7570d70b3adf682768e72dcf0ca24
Samuelwsz <Samuelxwsz@gmail.com>
2023-07-24 14:52:56 -0300

    att

---
+++ pages/faq.jsx
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function faqPage() {
+  return (
+    <>
+      <h3>Pagina da faq</h3>
+      <Link href="/">Ir para home</Link>
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,22 @@
+import dynamic from "next/dynamic"
+const HomeScreen = dynamic(() => import("../src/screens/HomeSreen/index"), {
+  ssr: false,
+})
+
+export default HomeScreen
+
+/*
+import Link from "../src/components/Link"
+import Footer from "../src/components/pattern/Footer"
+
 export default function HomePage() {
   return (
     <>
       <div>
         <h1>Alura Cases</h1>
+        <Link href="/faq">ir para faq</Link>
+        <Footer />
       </div>
     </>
   )
-}
+}*/
